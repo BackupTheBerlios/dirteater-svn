3ca1d6184a310fd75140db49e6518f06e21c94b2
chkorn <chkorn@ce24b671-e5f2-0310-a8aa-9a8824b830dc>
2005-04-14 14:22:03 +0000

    configuration is nearly complete now. error / message handling is now made in a good way but needs testing.
    more info if you view the diff :)

--- trunk/dirteater.py
+++ trunk/dirteater.py
@@ -13,7 +13,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 
+# USA
 #
 # $Id$
 
@@ -22,19 +23,19 @@ headurl = "$HeadURL: svn+ssh://svn.berlios.de/svnroot/repos/dirteater/trunk/dirt
 
 import os
 import sys
-import signal
 import types
 import string
 import threading
 import os.path
-import traceback
 import copy
 import logging
 import pprint
 import xml.dom.minidom
 
+from logging import handlers
 from xml.dom.minidom import Node
 from xml.dom.minidom import Element
+from xml.dom.NamedNodeMap import *
 from xml.parsers.xmlproc import xmlval, xmlproc
 from xml.parsers.xmlproc.utils import ErrorPrinter
 
@@ -42,11 +43,11 @@ from dirtErrHandle import *
 from dirtLib import *
 
 def main():
-	# Some globals and variables
 	global config
 	
 	config = {}
 	general_cfg = {}
+	plugin_cfg = {}
 	
 	options = [
 		"--config",
@@ -75,7 +76,13 @@ def main():
 		"--logtype",
 		"--verbose"
 	]
-	
+
+	logger = logging.getLogger("dirteater")
+	logformat = logging.Formatter("%(asctime)s %(levelname)s %(message)s", "%Y-%m-%d %H:%M:%S")
+	hdlr = logging.StreamHandler(sys.stdout)
+	hdlr.setFormatter(logformat)
+	logger.addHandler(hdlr)
+	logger.setLevel(logging.WARNING)
 
 	# Scan for commandline options. This is like the handling 
 	# of Gentoo's (http://www.gentoo.org) "emerge". 
@@ -89,35 +96,37 @@ def main():
 			for y in x[1:]:
 				if shortoptions.has_key(y):
 					if shortoptions[y] in cmd:
-						print "*** Warning: Redundant use of "+shortoptions[y]
+						logger.info("Redundant use of "+shortoptions[y])
 					else:
 						cmd.append(shortoptions[y])
 				else:
-					print "!!! Error: -"+y+" is an invalid shortoption."
+					logger.error(y+" is an invalid shortoption!")
 					sys.exit(1)
 		else:
 			cmd.append(x)
 	for x in cmd:
 		if len(x) >= 2 and x[0:2] == "--":
+			if x in opts:
+				logger.info("Redundant use of "+x)
 			if x in options:
 				opts.append(x)
 			else:
-				print "!!! Error "+x+" is an invalid option."
+				logger.error(x+" is an invalid option!")
 				sys.exit(1)
 		else:
 			opts.append(x)
 	
-	# "One option, one output and die" (tm)
 	if "--help" in opts: 
-		print "This is dirteater (http://dirteater.berlios.de). The information management system.\n"
+		print "This is dirteater (http://dirteater.berlios.de). The information management system.\n"""
 		print "These are the availibale command line options:\n"
 		print "--config <path>\t\t Specify configuration file manually"
 		print "--configdump \t\t Parse configuation file and print out all configuration variables"
 		print "--daemon \t\t Make dirteater daemonic"
-		print "--debug \t\t Degging output"
+		print "--debug \t\t Debugging output"
 		print "--help \t\t\t Display this help message"
 		print "--logtype \t\t Where do you want to log? See manual page for more information"
 		print "--module-help <module> \t Display help for a module and exit. Modules are named for example: 'output:hello'"
+		print "--nodtd \t\t Make no validation against a DTD"
 		print "--verbose \t\t Make output verbose"
 		print "--version \t\t Print out version information and exit\n"
 		print "Please report bugs to <dirteater-dev@lists.berlios.de>\n"
@@ -133,20 +142,20 @@ def main():
 		sys.exit()
 	elif "--module-help" in opts:
 		if len(opts) < 2:
-			print "!!! Error: You want to use --module-help, but no module was appended!"
+			logger.error("You want to use --module-help, but no module was appended!")
 			sys.exit(1)
 		elif opts[opts.index("--module-help")+1][0:2] == "--" or len(opts) <= 1:
-			print "!!! Error: You want to use --module-help, but no module was appended!"
+			logger.error("You want to use --module-help, but no module was appended!")
 			sys.exit(1)
 		else:
 			moduleinfo = string.split(opts[opts.index("--module-help")+1], ":")
 			if len(moduleinfo) < 1:
-				print "!!! Error: You have to specify the module you want help for in this syntax 'output:hello'"
+				logger.error("You have to specify the module you want help for in this syntax 'output:hello'")
 				sys.exit(1)
 			else: 
 				if moduleinfo[0] != "output" and moduleinfo[0] != "input":
 					print moduleinfo[0]
-					print "!!! Error: You have specified a wrong module type. You only can use 'input' or 'output'"
+					logger.error("You have specified a wrong module type. You only can use 'input' or 'output'")
 					sys.exit(1)
 				else: 
 					print "Help for "+moduleinfo[0]+" module '"+moduleinfo[1]+"':\n"
@@ -167,8 +176,6 @@ def main():
 	if not os.path.exists(homedir):
 		homedir = os.getcwd()
 		
-	# Get the systems global settings directory
-	# Try both /etc and the windows "all users" diretory
 	if os.path.isdir("/etc/"):
 		etcdir = "/etc"
 	elif os.getenv("ALLUSERSPROFILE") != "":
@@ -178,31 +185,29 @@ def main():
 
 	if "--config" in opts:
 		if len(opts) == 1:
-			print "!!! Error: You want to use --config, but no file was appended!"
+			logger.error("You want to use --config, but no file was appended!")
 			sys.exit(1)
 		elif opts[opts.index("--config")+1][0:2] == "--" or len(opts) <= 1:
-			print "!!! Error: You want to use --config, but no file was appended!"
+			logger.error("You want to use --config, but no file was appended!")
 			sys.exit(1)
 
 		if os.path.isfile(opts[opts.index("--config")+1]) is False:
-			print "!!! Error: Configuration file not found!"
+			logger.error("Configuration file not found!")
 			sys.exit()
 		else:
-			general_cfg['config'] = opts[opts.index("--config")+1]
+			general_cfg[u'config'] = opts[opts.index("--config")+1]
 	else:
 		if os.path.isfile(homedir+"/.dirt.xml"):
-			general_cfg['config'] = homedir+"/.dirt.xml"
+			general_cfg[u'config'] = homedir+"/.dirt.xml"
 		elif os.path.isfile(etcdir+"/dirt.xml"):
-			general_cfg['config'] = etcdir+"/dirt.xml"
+			general_cfg[u'config'] = etcdir+"/dirt.xml"
 		else: 
-			print "!!! Errror: No configuration file can be found!"
+			logger.error("No configuration file can be found!")
 			sys.exit()
 	
 	if "--nodtd" in opts: 
-		print "*** Warning: --nodtd is appended. Now no validation against a DTD is made! \
-		Only do this for debugging or testing of new features!"
+		logger.warning("--nodtd is appended. Now no validation against a DTD is made! Only do this for debugging or testing of new features!")
 	else:
-		# Validate XML
 		try: 
 			parser = xmlval.XMLValidator()
 			parser.set_error_handler(dtdErrHandle(parser))
@@ -215,7 +220,6 @@ def main():
 
 	subnode_cfg = {}
 	
-	# Get the complete general configuration
 	generalconfig = configuration.getElementsByTagName('general')
 	for mainnode in generalconfig: 
 		for subnode in mainnode.childNodes:
@@ -229,24 +233,53 @@ def main():
 						if len(string.strip(lastnode.data)) > 0:
 							general_cfg[lastnode.parentNode.nodeName] = xmlVarConvert(lastnode.data)
 				subnode_cfg = {}
-	
-	# Merge the general configuration into the complete config
-	config['general'] = general_cfg
 
-	# Overwrite settings
-	# REIMPLEMENTATION NEEDED!
-	#for set in opts:
-	#	if set in overwrite:
-	#		config[set[2:]] = True
-	
-	# Just give back the whole configuration and exit.
+	pluginconfig = configuration.getElementsByTagName('plugin')
+	for mainnode in pluginconfig: 
+		for subnode in mainnode.childNodes:
+			if subnode.nodeType == Node.ELEMENT_NODE:
+				pluginname = mainnode.getAttribute("name")
+				plugintype = mainnode.getAttribute("type")
+				pluginenable = mainnode.getAttribute("enable")
+
+				for lastnode in subnode.childNodes:
+					if len(string.strip(lastnode.data)) > 0:
+						subnode_cfg[subnode.nodeName] = lastnode.data
+
+		plugin_cfg[pluginname] = subnode_cfg
+		subnode_cfg = {}
+
+	config['general'] = general_cfg
+	config['plugin'] = plugin_cfg
+
+	# this is a crazy logging system. but is it secure?
+	log = {
+		"stdout":{"class":"StreamHandler","opts":""},
+		"file":{"class":"FileHandler", "opts":"'"+config['general']['logging']['file']+"'"},
+		"rotating-file":{"class":"handlers.RotatingFileHandler", \
+			"opts":"'"+config['general']['logging']['file'] \
+			+"', maxBytes='"+config['general']['logging']['size'] \
+			+"', backupCount='"+config['general']['logging']['count']+"'"},
+		"windowslog":{"class":"NTEventLogHandler", "opts":"'dirteater', logtype='Application'"},
+		"syslog":{"class":"handlers.SysLogHandler","opts":""}
+	}
+
+	logger.removeHandler(hdlr)
+	exec "user_hdlr = logging." \
+		+log[config['general']['logging']['type']]['class'] \
+		+"("+log[config['general']['logging']['type']]["opts"]+")"
+	user_hdlr.setFormatter(logformat)
+	logger.addHandler(user_hdlr)
+
+	if config['general']['logging']['debug'] is True: 
+		logger.setLevel(logging.DEBUG)
+	elif config['general']['logging']['verbose'] is True:
+		logger.setLevel(logging.INFO)
+
 	if "--configdump" in opts:
 		print "This is your configuration:\n"
 		pprint.pprint(config)
 		sys.exit()
-
-	### FROM HERE ON COMPLETE MESSAGE HANDLING IS MADE 
-	
 
 if __name__ == "__main__":
 	main()
